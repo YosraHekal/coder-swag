c205c965391ba5c88fd3538d016b11a625cb4127
Yosra H. Hekal <yu@Yosras-MacBook-Pro.local>
2018-07-05 12:04:38 +0200

    table view & cells

---
+++ coder.swag/Controller/CategoriesVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  coder.swag
+//
+//  Created by Yosra H. Hekal on 05.07.18.
+//  Copyright © 2018 Yosra H. Hekal. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
